7fbc9e6188ef5c73abbd54ed14f18d5c26cff2ec
Albaro Pereyra <141711575+albaropereyra22@users.noreply.github.com>
2023-08-25 00:28:34 -0700

    Hello Drogon

--- main.cc
+++ main.cc
@@ -1,9 +1,10 @@
 #include <drogon/drogon.h>
+
 int main() {
     //Set HTTP listener address and port
     drogon::app().addListener("0.0.0.0",80);
     //Load config file
-    //drogon::app().loadConfigFile("../config.json");
+    drogon::app().loadConfigFile("C:\\Users\\dell\\your_project_name\\config.json");
     //Run HTTP framework,the method will block in the internal event loop
     drogon::app().run();
     return 0;
