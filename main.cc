095c0bcbfb9f976c28db54ca0c12a1e0a37f8ba6
Albaro Pereyra <141711575+albaropereyra22@users.noreply.github.com>
2023-08-26 03:11:18 -0700

    removed filter error

--- main.cc
+++ main.cc
@@ -7,5 +7,6 @@ int main() {
     drogon::app().loadConfigFile("C:\\Users\\dell\\your_project_name\\config.json");
     //Run HTTP framework,the method will block in the internal event loop
     drogon::app().run();
+    drogon::app().setIntSignalHandler([&] {std::cerr << "Alarm! SIGINT!"; });
     return 0;
 }
