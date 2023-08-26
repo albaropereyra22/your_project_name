30fe0efbfa01ca274222434627f3c43516bbe758
Albaro Pereyra <141711575+albaropereyra22@users.noreply.github.com>
2023-08-26 03:34:35 -0700

    Catch SIGINT

--- main.cc
+++ main.cc
@@ -1,12 +1,13 @@
 #include <drogon/drogon.h>
 
 int main() {
+    //Catch SIGINT
+    drogon::app().setIntSignalHandler([&] {std::cerr << "Alarm! SIGINT!"; });
     //Set HTTP listener address and port
     drogon::app().addListener("0.0.0.0",80);
     //Load config file
     drogon::app().loadConfigFile("C:\\Users\\dell\\your_project_name\\config.json");
     //Run HTTP framework,the method will block in the internal event loop
     drogon::app().run();
-    drogon::app().setIntSignalHandler([&] {std::cerr << "Alarm! SIGINT!"; });
     return 0;
 }
