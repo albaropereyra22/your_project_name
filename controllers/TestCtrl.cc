dab22cfef8676b333a40e925bc935417f44d09ff
Albaro Pereyra <141711575+albaropereyra22@users.noreply.github.com>
2023-08-31 01:34:45 -0700

    Added logs.

--- controllers/TestCtrl.cc
+++ controllers/TestCtrl.cc
@@ -6,6 +6,9 @@ void TestCtrl::asyncHandleHttpRequest(const HttpRequestPtr& req,
 {
     //write your application logic here
     auto resp = HttpResponse::newHttpResponse();
+    //NOTE: The enum constant below is named "k200OK" (as in 200 OK), not "k2000K".
+    resp->setStatusCode(k200OK);
+    resp->setContentTypeCode(CT_TEXT_HTML);
     resp->setBody("<p>Hello, world!</p>");
     resp->setExpiredTime(0);
     callback(resp);
--- main.cc
+++ main.cc
@@ -1,13 +1,16 @@
 #include <drogon/drogon.h>
+using namespace drogon;
 
 int main() {
     //Catch SIGINT
-    drogon::app().setIntSignalHandler([&] {std::cerr << "Alarm! SIGINT!"; drogon::app().quit(); });
-    //Set HTTP listener address and port
-    drogon::app().addListener("0.0.0.0",80);
-    //Load config file
-    drogon::app().loadConfigFile("C:\\Users\\dell\\your_project_name\\config.json");
-    //Run HTTP framework,the method will block in the internal event loop
-    drogon::app().run();
+    app().setLogPath("C:\\Users\\dell\\your_project_name\\")
+         .setLogLevel(trantor::Logger::kWarn)
+         .setIntSignalHandler([&] {std::cerr << "Alarm! SIGINT!"; app().quit(); })
+         //Set HTTP listener address and port
+         .addListener("0.0.0.0",80)
+         //Load config file
+         .loadConfigFile("C:\\Users\\dell\\your_project_name\\config.json")
+         //Run HTTP framework,the method will block in the internal event loop
+         .run();
     return 0;
 }
