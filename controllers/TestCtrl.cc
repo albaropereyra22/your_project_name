3e373bd5740e669f7c22feb5d5f60a448317afde
Albaro Pereyra <141711575+albaropereyra22@users.noreply.github.com>
2023-08-31 00:22:20 -0700

    Reached hello world over controller.

--- controllers/TestCtrl.cc
+++ controllers/TestCtrl.cc
@@ -1,3 +1,4 @@
+/// The TestCtrl.cc file
 #include "TestCtrl.h"
 
 void TestCtrl::asyncHandleHttpRequest(const HttpRequestPtr& req,
@@ -5,9 +6,7 @@ void TestCtrl::asyncHandleHttpRequest(const HttpRequestPtr& req,
 {
     //write your application logic here
     auto resp = HttpResponse::newHttpResponse();
-    //NOTE: The enum constant below is named "k200OK" (as in 200 OK), not "k2000K".
-    resp->setStatusCode(k200OK);
-    resp->setContentTypeCode(CT_TEXT_HTML);
-    resp->setBody("Hello World!");
+    resp->setBody("<p>Hello, world!</p>");
+    resp->setExpiredTime(0);
     callback(resp);
 }
