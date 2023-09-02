2780a4a77e1c49f94f23271fadd00adf5cf511ae
Albaro Pereyra <141711575+albaropereyra22@users.noreply.github.com>
2023-09-01 23:22:10 -0700

    Removed logging

--- main.cc
+++ main.cc
@@ -14,13 +14,16 @@ int main() {
                 auto resp = HttpResponse::newHttpViewResponse("ListParameters.csp", data);
                 callback(resp);
             });
-    //Start app() select log path and log level
-    app().setLogPath("C:\\Users\\dell\\your_project_name\\")
-         .setLogLevel(trantor::Logger::kWarn)
-         //Catch SIGINT
-         .setIntSignalHandler([&] {std::cerr << "Alarm! SIGINT!"; app().quit(); })
+    //select log path and log level
+    // removed because it prevents debugging 
+    //.setLogPath("C:\\Users\\dell\\your_project_name\\")
+    //.setLogLevel(trantor::Logger::kWarn)
+      
+    //start app
+    //Catch SIGINT
+    app().setIntSignalHandler([&] {std::cerr << "Alarm! SIGINT!"; app().quit(); })
          //Set HTTP listener address and port
-         .addListener("0.0.0.0",80)
+         .addListener("0.0.0.0", 80)
          //Load config file
          .loadConfigFile("C:\\Users\\dell\\your_project_name\\config.json")
          //Run HTTP framework,the method will block in the internal event loop
