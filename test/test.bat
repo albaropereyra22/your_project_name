26f50b847258c5af6f25bc3003dcd97f2812ae02
Albaro Pereyra <141711575+albaropereyra22@users.noreply.github.com>
2023-09-01 23:04:47 -0700

    Added curl tests

---
+++ test/test.bat
@@ -0,0 +1,5 @@
+curl -v "http://localhost/"
+curl -v "http://localhost/test"
+curl -v "http://localhost/json"
+curl -v "http://localhost/demo/v1/user/xxxxx/info?token=xxxx"
+curl -v "http://localhost/list_para?p1=a&p2=b&p3=c" -d "p1=a"
